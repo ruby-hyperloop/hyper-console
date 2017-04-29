b681125e2af70fbdd39ed98731b646e6a5926d3d
catmando <mitch@catprint.com>
2017-04-29 16:22:27 -0400

    cleanup and fixes

--- lib/hyperloop/console/evaluate.rb
+++ lib/hyperloop/console/evaluate.rb
@@ -1,18 +1,20 @@
-class Evaluate < Hyperloop::ServerOp
-  param acting_user: nil, nils: true
-  param :target_id
-  param :sender_id
-  param :context
-  param string: nil
+module Hyperloop
+  module Console
+    class Evaluate < Hyperloop::ServerOp
+      param acting_user: nil, nils: true
+      param :target_id
+      param :sender_id
+      param :context
+      param string: nil
+      dispatch_to { Hyperloop::Application }
+    end
 
-  dispatch_to { Hyperloop::Application }
-end
-
-class Response < Hyperloop::ServerOp
-  param acting_user: nil, nils: true
-  param :target_id
-  param :kind
-  param :message
-
-  dispatch_to { Hyperloop::Application }
+    class Response < Hyperloop::ServerOp
+      param acting_user: nil, nils: true
+      param :target_id
+      param :kind
+      param :message
+      dispatch_to { Hyperloop::Application }
+    end
+  end
 end
